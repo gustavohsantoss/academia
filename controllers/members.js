eb64aa3846824031dcd228cb817bcb2de22c28c3
Gustavo Henrique Santos <gustavo_pildas@hotmail.com>
2021-03-20 14:43:14 -0300

    first commit

---
+++ controllers/members.js
@@ -0,0 +1,136 @@
+const fs = require('fs')
+const data = require('../data.json')
+const {age, date} = require('../utils')
+
+exports.index = function(req,res) {
+
+    return res.render("members/index", { members: data.members })
+}
+
+// show
+exports.show = function(req,res) {
+    // req.params.id = /:id
+    const { id } = req.params
+
+    const foundMember = data.members.find(function(member){
+        return member.id == id
+})
+
+
+    if (!foundMember) return res.send("Instrutor não encontrado")
+
+    const member = {
+        ...foundMember,
+        age: age(foundMember.birth),
+    }
+
+    return res.render("members/show", {member: member})
+}
+
+exports.create = function(req,res) {
+    return res.render('members/create')
+}
+// create
+exports.post = function(req,res){
+    
+    const keys = Object.keys(req.body)
+
+    for (key of keys) {
+        if (req.body[key] == "")
+            return res.send ("Todos os campos devem ser preenchidos")
+    }
+    
+    birth = Date.parse(req.body.birth)
+    let id = 1
+    const lastMember = data.members[data.members.length -1]
+
+    if (lastMember) {
+        id = lastMember.id + 1
+    }
+    data.members.push({
+        id,
+        ...req.body,
+        birth,
+    })
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
+        if (err) return res.send("File Error")
+    
+        return res.redirect("/members")
+    })
+
+
+
+    //return res.send(req.body)
+}
+// edit
+
+exports.edit = function(req, res) {
+     // req.params.id = /:id
+     const { id } = req.params
+
+     const foundMember = data.members.find(function(member){
+         return member.id == id
+ })
+ 
+ 
+     if (!foundMember) return res.send("Instrutor não encontrado")
+
+    const member = {
+        ... foundMember, 
+        birth: date(foundMember.birth)
+    }
+
+    
+    return res.render('members/edit', { member})
+}
+
+// put
+
+exports.put = function(req, res) {
+    const { id } = req.body
+    let index = 0
+    
+    const foundMember = data.members.find(function(member, foundIndex){
+        if (id == member.id) {
+            index = foundIndex
+            return true
+        }
+})
+
+
+    if (!foundMember) return res.send("Instrutor não encontrado")
+
+    const member = {
+        ...foundMember,
+        ...req.body,
+        birth: Date.parse(req.body.birth),
+        id: Number(req.body.id)
+    }
+
+    data.members[index] = member
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
+        if(err) return res.send("Erro")
+
+        return res.redirect(`/members/${id}`)
+    })
+}
+
+// delete
+
+exports.delete = function(req, res) {
+    const { id } = req.body
+
+    const filteredMembers = data.members.filter(function(member) {
+        return member.id != id
+    })
+
+    data.members = filteredMembers
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
+        if (err) return res.send("Erro")
+
+        return res.redirect("/members")
+    })
+}
